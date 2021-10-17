76564927a87da70bb3a5515519d3d12245dd98f9
Макс <@DESKTOP-T8IR23C>
2021-10-17 16:09:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	int m;
+	stringstream ss;
+	cin >> m;
+	while (m > 0) {
+		ss << m % 10;
+		m = m / 10;
+	}
+	ss >> m;
+	cout << m;
+	return 0;
+}
